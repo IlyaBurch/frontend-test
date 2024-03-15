2428412d2c3f99e0d8beac816d489316edcedf1c
IlyaBurch <ilyaburch@yandex.ru>
2024-03-15 15:01:59 +0300

    done v-for for forms

--- src/components/FormBuilder.vue
+++ src/components/FormBuilder.vue
@@ -1,33 +1,27 @@
 <template>
-  <div class="form-builder">
-    <!-- Должно собираться автоматически, а не руками как сейчас, согласно конфигу   -->
-    <form @submit.prevent="onSubmit">
-      <form-input label="Имя"  />
-      <form-select label="Гендер" :options="[{ value: 'male', text: 'Мужской' }, { value: 'female', text: 'Женский', selected: true }]" />
-      <form-radio label="Возраст" :options="[{ value: '10', text: 'Мужской' }, { value: '20', text: 'Женский', selected: true }]" />
-      <form-password label="Пароль" />
-
-      <button type="submit">Отправить</button>
-      <button type="reset">Стереть</button>
-    </form>
+  <div class="form-builder" v-for="field in data">
+    <FormItem :data="field"/>
   </div>
 </template>
 
 <script>
-import FormInput from "@/components/form-items/FormInput.vue";
-import FormSelect from "@/components/form-items/FormSelect.vue";
-import FormRadio from "@/components/form-items/FormRadio.vue";
-import FormPassword from "@/components/form-items/FormPassword.vue";
+
+import dataUsers from "@/json/form-config.json";
+import FormItem from "@/components/form-items/FormItem.vue";
 
 export default {
   name: "FormBuilder",
-
+  data(){
+    return {
+      data: dataUsers,
+    }
+  },
   methods: {
     onSubmit() {
       alert('Submit')
     }
   },
-  components: {FormPassword, FormRadio, FormSelect, FormInput}
+  components: {FormItem}
 }
 </script>
 
---
+++ src/components/form-items/FormItem.vue
@@ -0,0 +1,46 @@
+<template>
+      <p>{{ data.name }}</p>
+        <form @submit.prevent="onSubmit" >
+          <!-- <form-input :label="data.items.label"/> -->
+          <div v-for="item in data.items">
+            <form-input v-if="item.type === 'input'" :label="item.label" v-model="input"/>
+            <form-select v-if="item.type === 'select'" :label="item.label" :options="item.additional.options"/>
+            <form-radio v-if="item.type === 'radio'" :label="item.label" :options="item.additional.options"/>
+            <form-password v-if="item.type === 'password'" :label="item.label"/>
+          </div>
+
+          <button type="submit">Отправить</button>
+          <button type="reset">Стереть</button>
+        </form>
+</template>
+
+<script>
+import FormInput from "@/components/form-items/FormInput.vue";
+import FormSelect from "@/components/form-items/FormSelect.vue";
+import FormRadio from "@/components/form-items/FormRadio.vue";
+import FormPassword from "@/components/form-items/FormPassword.vue";
+
+export default {
+  name: "FormItem",
+
+  props: {
+    data: {
+      type: Object,
+        required: true
+    },
+  },
+  data() {
+    return{
+      input: '',
+      password: '',
+      select: '',
+      radio: '',
+    }
+  },
+  components: {FormPassword, FormRadio, FormSelect, FormInput}
+}
+</script>
+
+<style scoped>
+
+</style>
